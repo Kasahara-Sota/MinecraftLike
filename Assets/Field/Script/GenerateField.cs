5b85c931d05b40fc7e01a2a1b510e77f66ef96cf
KasaharaSota <souta.programer1201@gmail.com>
2025-01-22 23:50:26 +0900

    [update]地形生成時ブロックをジェネレーターで消す必要がなくなった

--- Assets/Field/Script/GenerateField.cs
+++ Assets/Field/Script/GenerateField.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class GenerateField : MonoBehaviour
@@ -9,34 +11,41 @@ public class GenerateField : MonoBehaviour
     float _beforeY;
     [SerializeField, Range(0.0001f, 1)] float _flatScale;
     float _beforeFlatScale;
+    [SerializeField] float _generateSpeed = 1;
     void Start()
+    {
+        //StartCoroutine(Generate());
+        Generate();
+    }
+    void Generate()
     {
         _beforeX = _xOrigin;
         _beforeY = _yOrigin;
         _beforeFlatScale = _flatScale;
-        for (int z = 0; z < 16; z++)
+        for (int z = 0; z < 16 * 4; z++)
         {
-            for (int x = 0; x < 16; x++)
+            for (int x = 0; x < 16 * 4; x++)
             {
                 float xValue = _xOrigin + x * _flatScale;
                 float yValue = _yOrigin + z * _flatScale;
                 float valueXZ = (int)(Mathf.PerlinNoise(xValue, yValue) * 10);
-                GameObject obj;
                 int n = (int)valueXZ + 64;
                 valueXZ /= 10;
+                GameObject obj;
                 for (int y = 0; y < n; y++)
                 {
                     obj = Instantiate(_obj, new Vector3(x, y, z), Quaternion.identity);
-                    if (Physics.Raycast(obj.transform.position, Vector3.down, out RaycastHit hit, 0.6f))
-                    {
-                        hit.collider.gameObject.GetComponent<Renderer>().enabled = false;
-                    }
                     obj.transform.SetParent(transform);
+                    //yield return new WaitForSeconds(_generateSpeed);
                 }
             }
+            //yield return null;
         }
-
     }
+    //IEnumerator Generate()
+    //{
+
+    //}
     void Update()
     {
         if (_beforeX != _xOrigin || _beforeY != _yOrigin || _beforeFlatScale != _flatScale)
