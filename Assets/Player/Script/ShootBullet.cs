28059860917b3ad07b140044c677a91b3c8dae31
KasaharaSota <souta.programer1201@gmail.com>
2025-02-07 03:35:14 +0900

    [add]プレイヤーが弾を撃つ処理

---
+++ Assets/Player/Script/ShootBullet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootBullet : MonoBehaviour
+{
+    [SerializeField] float _bulletSpeed = 5f;
+    [SerializeField] GameObject _bulletPrefab;
+    [SerializeField] GameObject _muzzle;
+    Camera _camera;
+    PlayerInput _playerInput;
+    void Start()
+    {
+        _camera = Camera.main;
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.actions["Interact"].performed += OnShoot;
+    }
+    void OnShoot(InputAction.CallbackContext context)
+    {
+        GameObject obj = Instantiate(_bulletPrefab, _muzzle.transform.position, Quaternion.identity);
+        obj.GetComponent<Rigidbody>().AddForce(_camera.transform.forward * _bulletSpeed, ForceMode.Impulse);
+    }
+}
