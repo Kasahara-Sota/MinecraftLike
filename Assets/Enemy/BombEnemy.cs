4202ed811c208d14adc1274198ac9ad81fd5799a
KasaharaSota <souta.programer1201@gmail.com>
2025-02-06 21:56:57 +0900

    [add]足場を破壊する弾を撃つ敵の追加

---
+++ Assets/Enemy/BombEnemy.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombEnemy : MonoBehaviour
+{
+    [SerializeField] GameObject _muzzle;
+    [SerializeField] GameObject _bullet;
+    [SerializeField] float _attackInterval = 10f;
+    [SerializeField] float _bulletSpeed = 5f;
+    float _timer;
+    GameObject _player;
+    private void Start()
+    {
+        _player = GameObject.FindAnyObjectByType<PlayerController>().gameObject;
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerBullet"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void Attack()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _attackInterval)
+        {
+            _timer = 0;
+            GameObject obj = Instantiate(_bullet,_muzzle.transform.position,Quaternion.identity);
+            obj.GetComponent<Rigidbody>().AddForce((_player.transform.position - _muzzle.transform.position) * _bulletSpeed,ForceMode.Impulse);
+        }
+    }
+
+}
