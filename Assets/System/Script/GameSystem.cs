ff52e8ab6b6f0ec2dd6eebfe4818ccab067560a2
KasaharaSota <souta.programer1201@gmail.com>
2025-02-10 23:38:38 +0900

    [update]ゲーム開始時にカーソルロック、ゲームオーバー時アンロック

--- Assets/System/Script/GameSystem.cs
+++ Assets/System/Script/GameSystem.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class GameSystem : MonoBehaviour
 {
     [SerializeField] int _fps = 60;
-    void Start()
+    GameOverChecker _gameOverChecker;
+    void Awake()
     {
         Application.targetFrameRate = _fps;
+        Cursor.lockState = CursorLockMode.Locked;
+        _gameOverChecker = FindAnyObjectByType<GameOverChecker>();
+        _gameOverChecker.OnGameOver += UnlockCursor;
+    }
+    void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
     }
 }
