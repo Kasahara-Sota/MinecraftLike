41f24b54c080b6559e7d5e33aa1b44a779012fae
KasaharaSota <souta.programer1201@gmail.com>
2025-02-12 00:19:36 +0900

    [update]ゲームオーバー時に背景をフェードアウト。ゲームオーバーを参照可能にする。

--- Assets/System/Script/GameOverChecker.cs
+++ Assets/System/Script/GameOverChecker.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 public class GameOverChecker : MonoBehaviour
 {
     [SerializeField] float height = -1.0f;
+    [SerializeField] GameObject _fadePanel;
     public event Action OnGameOver;
     GameObject _player;
-    bool _isGameOver = false;
+    static bool _isGameOver = false;
+    public static bool IsGameOver => _isGameOver;
     void Start()
     {
+        _isGameOver = false;
         _player = FindAnyObjectByType<PlayerController>().gameObject;
     }
 
@@ -17,6 +20,9 @@ public class GameOverChecker : MonoBehaviour
         if (!_isGameOver && height > _player.transform.position.y)
         {
             OnGameOver?.Invoke();
+            _player.GetComponent<PlayerController>().enabled = false;
+            _fadePanel.SetActive(true);
+            _fadePanel.GetComponent<Fade>().FadeOut();
             _isGameOver = true;
         }
     }
