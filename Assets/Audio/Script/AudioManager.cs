7518ff3026cd274a6ea1d10ecb5fe99a144c4988
KasaharaSota <souta.programer1201@gmail.com>
2025-02-11 20:48:03 +0900

    [Add]音を管理再生する

---
+++ Assets/Audio/Script/AudioManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] List<BGMs> _bgms;
+    [SerializeField] List<SEs> _ses;
+    public static AudioManager Audio;
+    private AudioSource _audioSource;
+    [Serializable]
+    public struct BGMs
+    {
+        public string Name;
+        public AudioClip BGM;
+    }
+    [Serializable]
+    public struct SEs
+    {
+        public string Name;
+        public AudioClip SE;
+    }
+    private void Awake()
+    {
+        if (Audio == null)
+        {
+            Audio = this;
+            DontDestroyOnLoad(this);
+            if (TryGetComponent<AudioSource>(out AudioSource audioSource))
+            {
+                _audioSource = audioSource;
+            }
+            else
+            {
+                _audioSource = gameObject.AddComponent<AudioSource>();
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void PlaySE(string name)
+    {
+        AudioClip audioClip = _ses.Find(x => x.Name == name).SE;
+        if (audioClip == null)
+        {
+            Debug.LogWarning($"ë∂ç›ÇµÇ»Ç¢å¯â âπ:{name}");
+            return;
+        }
+        _audioSource.PlayOneShot(audioClip);
+    }
+    public void PlayBGM(string name)
+    {
+        AudioClip audioClip = _bgms.Find(x => x.Name == name).BGM;
+        if (audioClip == null)
+        {
+            Debug.LogWarning($"ë∂ç›ÇµÇ»Ç¢BGM:{name}");
+            return;
+        }
+        _audioSource.clip = audioClip;
+        _audioSource.Play();
+        _audioSource.loop = true;
+    }
+}
